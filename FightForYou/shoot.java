83b0823ecf32832b5b36a875605a195bcf5ed751
pppandora <pandorary.secret@gmail.com>
2017-03-13 13:31:35 +0700

    Change coordinate offset in shoot

--- FightForYou/shoot.java
+++ FightForYou/shoot.java
@@ -16,7 +16,7 @@ public class shoot extends game
    public void act() 
     {
         // Add your action code here.
-      setLocation(getX()-50,getY());
+      setLocation(getX()-55,getY());
       if(isAtEdge()){
          getWorld().removeObject(this);
          return;
